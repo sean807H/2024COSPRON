6af66b22f1955475b103d5f39063219a23e0919d
김서연 <129020628+sean807H@users.noreply.github.com>
2024-07-22 12:22:09 +0900

    기초함수문제

---
+++ 2024COSPRON/monday.py
@@ -0,0 +1,45 @@
+#240722
+scores = [2024, 7, 22, 3, 4]
+
+#1
+def sum_list(num):
+    total = 0
+    for i in num:
+        total += i
+    return total
+
+print(sum_list(scores))   #2060
+
+def max_list(num1):
+    # return max(num1)
+    max_ = num1[0]
+    for i in num1:
+        if(i > max_):
+            max_ = i
+    return max_
+
+print(max_list(scores))   #2024
+
+def min_list(num2):
+    # return min(num2)
+    min_ = num2[0]
+    for i in num2:
+        if(i < min_):
+            min_ = i
+    return min_
+
+print(min_list(scores))   #3
+
+def count_odd(num3):
+    count = 0
+    for i in num3:
+        if(i%2!=0):
+            count+=1
+    return count
+
+print(count_odd(scores))   #10
+
+def make_list(num4):
+    return [0] * num4
+
+print(make_list(6))   #[0, 0, 0, 0, 0, 0]
