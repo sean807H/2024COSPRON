ea60888a266a318f2535a6a5891d19da2c30a5eb
김서연 <129020628+sean807H@users.noreply.github.com>
2024-07-22 11:28:21 +0900

    연습문제_다같이

---
+++ 2024COSPRON/july.py
@@ -0,0 +1,51 @@
+#240722
+
+# print("Hello world")
+
+# name = "이석민"
+# age = 27
+# print(f'안녕, 나는 {name}이야. 나이는 {age}살이야')
+
+# SVT = ["이석민", "전원우", "최승철"]
+# for i in SVT:
+#     print(i)
+
+# for i in range(0, len(SVT), 1):             #for i in range(len(SVT)): #0,1,2, ..., 플레이브 길이 전
+#     print(SVT[i])
+
+# for i in range(2, 9+1):
+#     for j in range(1, 9+1):
+#         print(f'{i} X {j} = {i*j}')
+#     print("-"*10)                             # shift + tap
+
+# for i in range(2,10,2):
+#     for j in range(1,10):
+#         print(f'{i} X {j} = {i*j}')
+
+# 중식 = [{'menu':'자장면', 'price':7000}, {'menu':'짬뽕', 'price':8000}, {'menu':'양장피', 'price':15000}]
+# print(중식[0]['price'])
+# for 음식 in 중식:
+#     print(음식['menu'])       #print(음식.get('menu'))
+#     print(음식['price'])
+
+# def get_total_price(dictionary):
+#     total = 0
+#     for 하나씩 in dictionary:
+#         total += 하나씩['price']
+#     return total
+
+# print(get_total_price(중식))
+
+# lunch = {"핫도그":1000, "라면":900, "치킨":10000}
+# for menu, price in lunch.items():
+#     print(f'{menu} : {price}')
+
+# def lunch(menu):
+#     total_price = 0
+#     for value in menu.values():
+#         total_price += value
+#     return total_price
+
+# print(lunch({"핫도그":1000, "라면":900, "치킨":10000}))
+
+
