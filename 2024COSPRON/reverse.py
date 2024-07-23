a03fea5d639465dca688593304579b16907c8cf3
김서연 <129020628+sean807H@users.noreply.github.com>
2024-07-24 08:17:43 +0900

    COS PRO 2급 기출문제 1차 5번

---
+++ 2024COSPRON/reverse.py
@@ -0,0 +1,10 @@
+def solution(arr):
+    left, right = 0, len(arr)-1
+
+    while left<right:
+        arr[left], arr[right] = arr[right], arr[left]
+        left += 1
+        right -= 1
+    return arr
+
+print(solution([1,4,2,3]))
